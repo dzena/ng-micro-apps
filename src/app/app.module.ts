21cc8503cb2d943957fe429c4e71511371c13168
Milan Dzenovljanovic <md@innovation.rocks>
2019-08-20 14:01:52 +0200

    blah

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { App1SharedModule } from "../../projects/app1/src/app/app.module";
-import { App2SharedModule } from "../../projects/app2/src/app/app.module";
+import { App1SharedModule } from '../../projects/app1/src/app/app.module';
+import { App2SharedModule } from '../../projects/app2/src/app/app.module';
 import { NavComponent } from './nav/nav.component';
 
-@NgModule({
+@NgModule( {
   declarations: [
     AppComponent,
     NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    App1SharedModule.forRoot(),
-    App2SharedModule.forRoot()
+    AppRoutingModule
+    // App1SharedModule.forRoot(),
+    // App2SharedModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
-export class AppModule { }
+} )
+export class AppModule {
+}
